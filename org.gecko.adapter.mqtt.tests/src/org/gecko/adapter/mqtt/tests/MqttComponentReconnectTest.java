712e05afb0d3c8487318ba69bef2f3269fb63ee5
Guido Grune <g.grune@datainmotion.com>
2024-07-03 14:18:19 +0200

    Update tests to osgi test
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- org.gecko.adapter.mqtt.tests/src/org/gecko/adapter/mqtt/tests/MqttComponentReconnectTest.java
+++ org.gecko.adapter.mqtt.tests/src/org/gecko/adapter/mqtt/tests/MqttComponentReconnectTest.java
@@ -5,21 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.eclipse.paho.mqttv5.common.MqttException;
+import org.gecko.moquette.broker.MQTTBroker;
 import org.gecko.osgi.messaging.Message;
 import org.gecko.osgi.messaging.MessagingConstants;
 import org.gecko.osgi.messaging.MessagingService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -32,10 +28,6 @@ import org.osgi.test.junit5.cm.ConfigurationExtension;
 import org.osgi.test.junit5.context.BundleContextExtension;
 import org.osgi.test.junit5.service.ServiceExtension;
 
-import io.moquette.broker.Server;
-import io.moquette.broker.config.IConfig;
-import io.moquette.broker.config.MemoryConfig;
-
 @RequireConfigurationAdmin
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(ServiceExtension.class)
@@ -45,23 +37,8 @@ public class MqttComponentReconnectTest {
 
 	private static final String TOPIC = "publish.junit." + UUID.randomUUID();
 	private static final String BROKER_URL = "tcp://localhost:2183";
-	private Server server;
 	private CountDownLatch messageLatch;
-	private MemoryConfig config;
 
-	@BeforeEach
-	public void setup() throws Exception {
-		server = new Server();
-		config = new MemoryConfig(new Properties());
-		config.setProperty(IConfig.HOST_PROPERTY_NAME, "127.0.0.1");
-		config.setProperty(IConfig.PORT_PROPERTY_NAME, "2183");
-		server.startServer(config);
-	}
-
-	@AfterEach
-	public void teardown() throws MqttException, IOException {
-		server.stopServer();
-	}
 
 	/**
 	 * Tests reconnect client
@@ -69,15 +46,20 @@ public class MqttComponentReconnectTest {
 	 * @throws Exception
 	 */
 	@Test
+	@WithFactoryConfiguration(factoryPid = "MQTTBroker", location = "?", name = "broker", properties = {
+			@Property(key = MQTTBroker.HOST, value = "localhost"),
+			@Property(key = MQTTBroker.PORT, value = "2183") })
 	@WithFactoryConfiguration(factoryPid = "MQTTService", location = "?", name = "read", properties = {
 			@Property(key = MessagingConstants.PROP_BROKER, value = BROKER_URL) })
 	@WithFactoryConfiguration(factoryPid = "MQTTService", location = "?", name = "write", properties = {
 			@Property(key = MessagingConstants.PROP_BROKER, value = BROKER_URL) })
-	public void testReconnect(@InjectService(cardinality = 0) ServiceAware<MessagingService> read,
-			@InjectService(cardinality = 0) ServiceAware<MessagingService> write) throws Exception {
+	public void testReconnect(@InjectService(cardinality = 0) ServiceAware<MQTTBroker> bAware,
+			@InjectService(cardinality = 0) ServiceAware<MessagingService> read, @InjectService(cardinality = 0) ServiceAware<MessagingService> write) throws Exception {
 
 		messageLatch = new CountDownLatch(1);
 
+		MQTTBroker broker = bAware.getService();
+		
 		MessagingService readMessagingService = read.getService();
 		assertNotNull(readMessagingService);
 		readMessagingService.subscribe(TOPIC).forEach(m -> handle(m));
@@ -94,12 +76,12 @@ public class MqttComponentReconnectTest {
 			assertTrue(publisher.isPublishing);
 
 			// stop Server
-			server.stopServer();
+			broker.stop();
 			publisher.waitError(3);
 			assertFalse(publisher.isPublishing);
 
 			// start Server
-			server.startServer(config);
+			broker.start();
 
 			// waiting for reconnect
 			publisher.waitSuccess(3);
@@ -113,9 +95,8 @@ public class MqttComponentReconnectTest {
 
 	private void handle(Message msg) {
 		String v = new String(msg.payload().array());
-		System.out.println("Message: " + v);
-		messageLatch.countDown();
 		assertEquals("42", v);
+		messageLatch.countDown();
 	}
 
 	private static final class TestPublisher implements Runnable {
@@ -123,13 +104,13 @@ public class MqttComponentReconnectTest {
 		private boolean isPublishing;
 		private final CountDownLatch successLatch;
 		private CountDownLatch errorLatch;
-	
+
 		private TestPublisher(MessagingService messagingService) {
 			this.successLatch = new CountDownLatch(1);
 			this.errorLatch = new CountDownLatch(1);
 			this.messagingService = messagingService;
 		}
-	
+
 		@Override
 		public void run() {
 			isPublishing = true;
@@ -152,11 +133,11 @@ public class MqttComponentReconnectTest {
 				}
 			}
 		}
-	
+
 		private void waitError(int seconds) throws InterruptedException {
 			errorLatch.await(seconds, TimeUnit.SECONDS);
 		}
-	
+
 		private void waitSuccess(int seconds) throws InterruptedException {
 			successLatch.await(seconds, TimeUnit.SECONDS);
 		}
